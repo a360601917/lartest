8e57e52152bdb3a2f441ca295d839243df693204
wl1 <360601917@qq.com>
2018-09-10 17:58:30 +0800

    a

--- app/Handlers/SendSmsHandler.php
+++ app/Handlers/SendSmsHandler.php
@@ -9,28 +9,53 @@ class SendSmsHandler {
 
   public $key = 'verify_code_';
   public $error = [];
+  private $template = ['ch' => ['template' => 'SMS_138390081'], 'au' => ['prefix' => 00, 'template' => 'SMS_138062207']];
 
-  public function code($phone,$time) {
+  public function code($phone, $time) {
     $easySms = app('easysms');
-    $code = str_pad(random_int(1, 9999),4,0,STR_PAD_LEFT);
+    $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
     try {
+      $template= $this->checkPhone($phone);
+      //$code = 1234;
+      $result = $easySms->send($template['phone'], [
+          'template' => $template['template'],
+          'data' => [
+              'code' => $code
+          ],
+      ]);
       
-//      $result = $easySms->send($phone, [
-//          'template' => 'SMS_138390081',
-//          'data' => [
-//              'code' => $code
-//          ],
-//      ]);
-      $code=1234;
-      
-      $key= $this->key.str_random(10);
-      $time=$time?:now()->addMinutes(10);
+
+      $key = $this->key . str_random(10);
+      $time = $time ?: now()->addMinutes(10);
       Cache::put($key, ['verify_code' => $code, 'phone' => $phone], $time);
       return $key;
     } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
       $message = $exception->getException('aliyun')->getMessage();
       $this->error[] = $message;
     }
+  }
+
+  public function checkPhone($phone) {
+    switch (substr($phone, 0, 2)) {
+      case '61':
+        $data = [
+            'phone' => $this->template['au']['prefix'] . $phone,
+            'template' => $this->template['au']['template'],
+        ];
+        break;
+      case '00':
+        $data = [
+            'phone' => $phone,
+            'template' => $this->template['au']['template'],
+        ];
+        break;
+      default:
+        $data = [
+            'phone' => $phone,
+            'template' => $this->template['ch']['template'],
+        ];
+    }
+    return $data;
   }
 
 }
