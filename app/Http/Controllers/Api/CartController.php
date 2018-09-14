005953d85e72ee1e495e420bf00056e47b6f2356
wl1 <360601917@qq.com>
2018-09-14 17:53:40 +0800

    a

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -13,35 +13,68 @@ use App\Models\Goods;
 class CartController extends Controller {
 
   public function index() {
-    $user=new User;
-    $user_id= $user->tokenId();
-    $carts=Cart::where('user_id',$user_id)->get();
+    $user = new User;
+    $user_id = $user->tokenId();
+    $carts = Cart::where('user_id', $user_id)->get();
     return $this->response->collection($carts, new CartTransformer);
   }
-  
-  public function store(CartRequest $request){
-    if($carts=$request->carts){
-      $carts=json_decode($carts,1);
-      $goods_ids= array_column($carts, 'goods_id');
-      if(!$goods_ids){
+
+  public function store(CartRequest $request) {
+    if ($carts = $request->carts) {
+      $carts = json_decode($carts, 1);
+      $goods_ids = array_column($carts, 'goods_id');
+      if (!$goods_ids) {
         return $this->response->error('没给商品', 404);
       }
-      $goodss=Goods::whereIn('id',$goods_ids)->get()->toArray();
-      if(count($goodss)!=count($goods_ids)){
-        $diff= array_diff($goods_ids, array_column($goodss, 'id'));
-        return $this->response->error(implode(',', $diff).'商品不存在', 404);
+      $goodss = Goods::whereIn('id', $goods_ids)->get()->toArray();
+      if (count($goodss) != count($goods_ids)) {
+        $diff = array_diff($goods_ids, array_column($goodss, 'id'));
+        return $this->response->error(implode(',', $diff) . '商品不存在', 404);
       }
-      $user_id=(new User)->tokenId();
-      array_walk($carts, function(&$v) use ($user_id){
-        $v['user_id']=$user_id;
-      });
+
+      foreach ($carts as &$v) {
+        $v = $this->addUserId($v);
+      }
+      unset($v);
+
       Cart::insert($carts);
       return $this->response->created();
-      
-    }else{
-      
+    } else {
+      $carts = $request->only('goods_id', 'quantity');
+      $carts = $this->addUserId($carts);
+      Cart::insert($carts);
+      return $this->response->created();
     }
-    
+  }
+
+  public function update(CartRequest $request, Cart $cart) {
+    if ($quantity = $request->quantity) {
+      $cart->quantity = $quantity;
+      $cart->save();
+      return $this->response->noContent();
+    } elseif ($action = $request->action) {
+      switch (strtolower($action)) {
+        case 'add':
+          $cart->increment('quantity');
+          break;
+        case 'reduce':
+          $cart->decrement('quantity');
+          break;
+      }
+      $cart->save();
+      return $this->response->noContent();
+    }
+  }
+
+  public function destroy(Cart $cart) {
+    $cart->delete();
+    return $this->response->noContent();
+  }
+
+  private function addUserId($arr) {
+    $user_id = (new User)->tokenId();
+    $arr['user_id'] = $user_id;
+    return $arr;
   }
 
 }
--- app/Http/Requests/Api/CartRequest.php
+++ app/Http/Requests/Api/CartRequest.php
@@ -4,28 +4,31 @@ namespace App\Http\Requests\Api;
 
 //use Illuminate\Foundation\Http\FormRequest;
 
-class CartRequest extends FormRequest
-{
+class CartRequest extends FormRequest {
 
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-      switch ($this->method()){
-        case 'POST':
-          $rule=[
-              'carts'=>'required_without:goods_id|json',
-              //'user_id'=>'required_without:carts|numeric',
-              'goods_id'=>'required_without:carts|numeric|exists:goods,id',
-              'quantity'=>'required_without:carts|integer|min:1',
-          ];
-          break;
-      }
-      return $rule;
-
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array
+   */
+  public function rules() {
+    switch ($this->method()) {
+      case 'POST':
+        $rule = [
+            'carts' => 'required_without:goods_id|json',
+            //'user_id'=>'required_without:carts|numeric',
+            'goods_id' => 'required_without:carts|numeric|exists:goods,id',
+            'quantity' => 'required_without:carts|integer|min:1',
+        ];
+        break;
+      case 'PATCH':
+        $rule=[
+            'quantity'=>'required_without:action|integer|min:1',
+            'action'=>'required_without:quantity|string',
+        ];
+        break;
     }
+    return $rule;
+  }
+
 }
