29e5c35daed8d8d1dc337d32020a639ca738ea32
wl1 <360601917@qq.com>
2018-11-05 14:43:48 +0800

    a

--- routes/api.php
+++ routes/api.php
@@ -26,44 +26,52 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers\Api', 'middleware' => 
   $api->group(['middleware' => 'api.throttle', 'limit' => 10, 'expires' => 1], function($api) {
     //注删
     $api->post('user', 'User\UserController@store')->name('api.user.store');
-    
-    //图片难证码
-    $api->post('captchas', 'Auth\CaptchasController@store')->name('api.captchas.store');
-    //验证码
-    $api->post('verifyCode', 'Auth\VerifyCodeController@store')->name('api.verifyCode.store');
-    //刷新token
-    $api->put('authorizations/update', 'Auth\AuthorizationsController@update')->name('api.authorizations.update');
-    //删除token
-    $api->delete('authorizations/delete', 'Auth\AuthorizationsController@delete')->name('api.authorizations.delete');
-    //登陆
-    $api->post('authorizations', 'Auth\AuthorizationsController@store')->name('api.authorizations.store');
-    // 第三方登录
-    $api->post('socials/{social_type}/authorizations', 'Auth\AuthorizationsController@socialStore')
-            ->name('api.socials.authorizations.store');
+    //auth
+    $api->group(['namespace'=>'Auth'],function($api){
+      //图片难证码
+      $api->post('captchas', 'CaptchasController@store')->name('api.captchas.store');
+      //验证码
+      $api->post('verifyCode', 'VerifyCodeController@store')->name('api.verifyCode.store');
+      //刷新token
+      $api->put('authorizations/update', 'AuthorizationsController@update')->name('api.authorizations.update');
+      //删除token
+      $api->delete('authorizations/delete', 'AuthorizationsController@delete')->name('api.authorizations.delete');
+      //登陆
+      $api->post('authorizations', 'AuthorizationsController@store')->name('api.authorizations.store');
+      // 第三方登录
+      $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
+              ->name('api.socials.authorizations.store');
+    });
+
   });
   
   
   $api->group(['middleware'=>'api.throttle','limit'=>60,'expires'=>1],function($api){
     //无须登陆
     //商品
-    $api->get('goods','Goods\GoodsController@index')->name('goods.index');
-    $api->get('goods/{goods}','Goods/GoodsController@show')->name('goods.show');
-    //$api->resource('goods','GoodsController',['only'=>['index','show']]);
-    //分类
-    $api->get('category','Category\CategoryController@index')->name('category.index');
-    $api->get('category/{category}','Category\CategoryController@show')->name('category.show');
-    //广告
-    $api->get('advertising','Advertising\AdvertisingController@index')->name('advertising.index');
-    $api->get('advertising/{advertising}','Advertising\AdvertisingController@show')->name('advertising.show');
+    $api->group(['namespace'=>'Goods'],function($api){
+      $api->get('goods','GoodsController@index')->name('goods.index');
+      $api->get('goods/{goods}','GoodsController@show')->name('goods.show');
+    });
 
-    
+    //分类
+    $api->group(['namespace'=>'Category'],function($api){
+      $api->get('category','CategoryController@index')->name('category.index');
+      $api->get('category/{category}','CategoryController@show')->name('category.show');
+    });
+    //广告
+    $api->group(['namespace'=>'Advertising'],function($api){
+      $api->get('advertising','AdvertisingController@index')->name('advertising.index');
+      $api->get('advertising/{advertising}','AdvertisingController@show')->name('advertising.show');
+    });
+
     
     //须登陆
     $api->group(['middleware'=>'api.auth'],function($api){
       //购物车
-      $api->resource('cart','CartController');
+      $api->resource('cart','Cart\CartController');
       //下单
-      $api->resource('orders','OrderController');
+      $api->resource('orders','Order\OrderController');
     });
     
   });
